f26ed414649b6097fc55d98310e929bc07123e84
Gonzalo15 <gmontero005@ikasle.ehu.es>
2016-11-08 12:16:34 +0100

    gb

--- src/Camarero.java
+++ src/Camarero.java
@@ -1,5 +1,11 @@
 import jade.core.Agent;
 
 public class Camarero extends Agent {
-
+	
+	 protected void setup() {
+	 } 
+	 
+	 protected void action(){
+		 
+	 }
 }
--- src/PartyAgent.java
+++ src/PartyAgent.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
 
 public class PartyAgent extends Agent {
-	int y;
+	
+	 protected void setup() {
+		 System.out.println("Hola! PartyAgent "+getAID().getName()+" esta listo.");
+		 DFAgentDescription host = new DFAgentDescription();
+		 
+		 
+	 } 
+	 
+	 private class Saludar extends Behaviour{
 
+		@Override
+		public void action() {
+			
+			
+		}
+
+		@Override
+		public boolean done() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		 
+	 }
 }
