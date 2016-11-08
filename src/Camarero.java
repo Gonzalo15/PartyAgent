7175f48e12b1e44e33b0a3fd14f571db04560c6f
egibar <aeguibar006@ikasle.ehu.es>
2016-11-08 16:49:35 +0100

    Camarero sirve comida y bebida

--- src/Camarero.java
+++ src/Camarero.java
@@ -3,9 +3,70 @@ import jade.core.Agent;
 public class Camarero extends Agent {
 	
 	 protected void setup() {
+         registerCamarero(getLocalName());
+
+         Random rnd = new Random();
+         int wakeTime = (int) (rnd.nextDouble()); //AJUSTAR BIEN EL RANDOM
+         blockingReceive(wakeTime);
+         addBehaviour(new TickerBehaviour(myAgent, 3000){) {
+             @Override
+             protected void onTick() {
+                 System.out.println("[tickerbehaviour] "+getLocalName() );
+                 switch(pasos){
+
+                     case 1:
+                         try {
+
+                             EnviarMensaje("Le apetece algo de comer");
+                         } catch (FIPAException fe) {
+                             fe.printStackTrace();
+                         }
+                         break;
+
+
+                     case 2:
+                         try {
+
+                             EnviarMensaje("Le apetece algo de beber");
+                         } catch (FIPAException fe) {
+                             fe.printStackTrace();
+                         }
+                         break;
+                 }
+                 pasos++;
+
+             }
+
+
+             }
+
+         });
+
+         addBehaviour(new LLEnaBehaviour());
+
+        void EnviarMensaje(String s){
+            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+            msg.setContent(s);
+            msg.setSender(myAgent.getAID());
+           // partyHost = DFService.search(myAgent, template)[0].getName();
+           // msg.addReceiver(partyHost);
+
+            for (DFAgentDescription df : DFService.search(myAgent, template2)) {
+                msg.addReceiver(df.getName());
+            }
+
+            myAgent.send(msg);
+
+     }
 	 } 
 	 
 	 protected void action(){
 		 
 	 }
+
+    @Override
+    protected void takeDown() {
+        System.out.println(getLocalName()+" se marcho de la fiesta");
+        this.doDelete();
+    }
 }
